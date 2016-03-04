43919f8a3d6b486f525024076f648091716a94f7
Don Denton <don@happycollision.com>
2016-03-04 13:55:49 -0600

    Adds relative links and description for testing.

--- presets/template81.js
+++ presets/template81.js
@@ -12,6 +12,15 @@
     }
   },
   {
+    "name":"Small Start - Include Assets 2",
+    "type":"preset",
+    "data": {
+      "video": "http://mediarobotvideo.s3.amazonaws.com/help/basic/customizing-text.mp4",
+      "xml": "sample-city-pro-preview.xml",
+      "description": "This preset's xml was set relative to the menu json file."
+    }
+  },
+  {
     "name":"Two whole slides! - No Assets",
     "type":"preset",
     "data": {
