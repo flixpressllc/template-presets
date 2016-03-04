fb468eded82dce8ae0693f9a4fb82e8c9ec819e0
Don Denton <don@happycollision.com>
2016-03-04 14:06:41 -0600

    Update json to use template folder

--- presets/template81.js
+++ presets/template81.js
@@ -4,19 +4,11 @@
     "type":"section"
   },
   {
-    "name":"Small Start - Include Assets",
-    "type":"preset",
-    "data": {
-      "video": "http://mediarobotvideo.s3.amazonaws.com/help/basic/customizing-text.mp4",
-      "xml": "/templates/presets/sample-city-pro-preview.xml"
-    }
-  },
-  {
     "name":"Small Start - Include Assets 2",
     "type":"preset",
     "data": {
       "video": "http://mediarobotvideo.s3.amazonaws.com/help/basic/customizing-text.mp4",
-      "xml": "sample-city-pro-preview.xml",
+      "xml": "81/sample-city-pro-preview.xml",
       "description": "This preset's xml was set relative to the menu json file."
     }
   },
@@ -25,7 +17,7 @@
     "type":"preset",
     "data": {
       "video": "http://mediarobotvideo.s3.amazonaws.com/help/basic/customizing-text.mp4",
-      "xml": "/templates/presets/placeholder.xml"
+      "xml": "81/placeholder.xml"
     }
   }
 ]
