f7eb6d8fabc51877db860d37bbf1f56f9fa210c5
Don Denton <don@happycollision.com>
2016-03-23 20:00:57 -0500

    Beginning of learning presets.
    These will probably go into another branch eventually, and stay there.

---
+++ presets/template53.js
@@ -0,0 +1,15 @@
+[
+  {
+    "name":"Template 53 Learning Presets",
+    "type":"section"
+  },
+  {
+    "name":"Untouched",
+    "type":"preset",
+    "data": {
+      "xml": "53/untouched.xml",
+      "video": "http://files.flixpress.com/1075982_1553968_P.mp4",
+      "description": "<p>This XML is straight from the server to a \"preset\" file. No modifications were made to the xml at all. There were 5 images. Image 5 was used in the second slide (which was layout 7). The other four were used in multi-image layout 16. Images 1, 2, and 4 had captions, I left #3 blank. User text was entered on the first two slides.</p><p>Strangely, some of the captions and text input were converted to dates. This happened BEFORE I got the XML, so it is acually reflected there.</p>"
+    }
+  }
+]
